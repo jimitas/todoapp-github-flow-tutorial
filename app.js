c847d77bf2a347b3a28280bac9e41fd9dbb3ef64
jimitas <nonajun805@gmail.com>
2023-08-09 09:44:04 +0900

    完了未完了の実装が完了しました

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ $("#addTodo").click(function () {
   const inputTodo = $("input").val();
   $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
 
-  $("input").val("")
+  $("input").val("");
+});
+
+$(document).on("change", "input[type=checkbox]", function () {
+  if ($(this).is(":checked")) {
+    $(this).parent().css("text-decoration", "line-through");
+  } else {
+    $(this).parent().css("text-decoration", "none");
+  }
 });
